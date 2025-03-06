418e68eaeaa5a8ba083d0238c42dbcb8e31cca28
remuruTempest <55364136+remuruTempest@users.noreply.github.com>
2025-03-06 16:25:16 -0700

    Update index.jsx

--- foodrecipe/src/context/index.jsx
+++ foodrecipe/src/context/index.jsx
@@ -1,4 +1,10 @@
 
 
 
-export default function ContextProvider({ children }) { }
+export default function ContextProvider({ children }) { }export default function ContextProvider({ children }) { 
+//States here
+
+ return(
+// Codes here
+ );
+}
