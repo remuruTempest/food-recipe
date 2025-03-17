952fb39c7ecc2849c1f11a531cbc4ecee4d92f0d
remuruTempest <55364136+remuruTempest@users.noreply.github.com>
2025-03-16 23:48:50 -0600

    Update index.jsx

--- foodrecipe/src/components/index.jsx
+++ foodrecipe/src/components/index.jsx
@@ -19,6 +19,62 @@ export default function ContextProvider({ children }) {
                     <li>recipe #4 </li>
 
                  </ol>
+                 <ol>
+                    <li>recioe #1</li>
+                    <li>recioe #2</li>
+                    <li>recioe #3</li>
+                    <li>recipe #4 </li>
+
+                 </ol>
+                 <ol>
+                    <li>recioe #1</li>
+                    <li>recioe #2</li>
+                    <li>recioe #3</li>
+                    <li>recipe #4 </li>
+
+                 </ol>
+                 <ol>
+                    <li>recioe #1</li>
+                    <li>recioe #2</li>
+                    <li>recioe #3</li>
+                    <li>recipe #4 </li>
+
+                 </ol>
+                 <ol>
+                    <li>recioe #1</li>
+                    <li>recioe #2</li>
+                    <li>recioe #3</li>
+                    <li>recipe #4 </li>
+
+                 </ol>
+                 <ol>
+                    <li>recioe #1</li>
+                    <li>recioe #2</li>
+                    <li>recioe #3</li>
+                    <li>recipe #4 </li>
+
+                 </ol>
+                 <ol>
+                    <li>recioe #1</li>
+                    <li>recioe #2</li>
+                    <li>recioe #3</li>
+                    <li>recipe #4 </li>
+
+                 </ol>
+                 <ol>
+                    <li>recioe #1</li>
+                    <li>recioe #2</li>
+                    <li>recioe #3</li>
+                    <li>recipe #4 </li>
+
+                 </ol>
+                 <ol>
+                    <li>recioe #1</li>
+                    <li>recioe #2</li>
+                    <li>recioe #3</li>
+                    <li>recipe #4 </li>
+
+                 </ol>
             </div>
         </div>
         <div></div>
