3159519ae1e79c4930b88485e189f2e378e3dda4
remuruTempest <55364136+remuruTempest@users.noreply.github.com>
2025-03-18 16:32:26 -0600

    Update index.jsx

--- foodrecipe/src/context/index.jsx
+++ foodrecipe/src/context/index.jsx
@@ -76,8 +76,75 @@ export default function ContextProvider({ children }) {
 
                  </ol>
             </div>
+            <div>
+                 <h2>Name:</h2>
+                 <h3>recipe</h3>
+                 <ol>
+                    <li>recioe #1</li>
+                    <li>recioe #2</li>
+                    <li>recioe #3</li>
+                    <li>recipe #4 </li>
+
+                 </ol>
+                 <ol>
+                    <li>recioe #1</li>
+                    <li>recioe #2</li>
+                    <li>recioe #3</li>
+                    <li>recipe #4 </li>
+
+                 </ol>
+                 <ol>
+                    <li>recioe #1</li>
+                    <li>recioe #2</li>
+                    <li>recioe #3</li>
+                    <li>recipe #4 </li>
+
+                 </ol>
+                 <ol>
+                    <li>recioe #1</li>
+                    <li>recioe #2</li>
+                    <li>recioe #3</li>
+                    <li>recipe #4 </li>
+
+                 </ol>
+                 <ol>
+                    <li>recioe #1</li>
+                    <li>recioe #2</li>
+                    <li>recioe #3</li>
+                    <li>recipe #4 </li>
+
+                 </ol>
+                 <ol>
+                    <li>recioe #1</li>
+                    <li>recioe #2</li>
+                    <li>recioe #3</li>
+                    <li>recipe #4 </li>
+
+                 </ol>
+                 <ol>
+                    <li>recioe #1</li>
+                    <li>recioe #2</li>
+                    <li>recioe #3</li>
+                    <li>recipe #4 </li>
+
+                 </ol>
+                 <ol>
+                    <li>recioe #1</li>
+                    <li>recioe #2</li>
+                    <li>recioe #3</li>
+                    <li>recipe #4 </li>
+
+                 </ol>
+                 <ol>
+                    <li>recioe #1</li>
+                    <li>recioe #2</li>
+                    <li>recioe #3</li>
+                    <li>recipe #4 </li>
+
+                 </ol>
+            </div>
         </div>
-        <div></div>
+        
 
         </div>
     );
