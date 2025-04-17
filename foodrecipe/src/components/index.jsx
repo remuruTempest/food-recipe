c59f44daa9aa384e9643dd71e715f856725aba49
remuruTempest <55364136+remuruTempest@users.noreply.github.com>
2025-04-17 06:50:38 -0600

    Update index.jsx

--- foodrecipe/src/components/index.jsx
+++ foodrecipe/src/components/index.jsx
@@ -157,6 +157,13 @@ export default function ContextProvider({ children }) {
                     <li>recipe #4 </li>
 
                  </ol>
+                 <ol>
+                    <li>recioe #1</li>
+                    <li>recioe #2</li>
+                    <li>recioe #3</li>
+                    <li>recipe #4 </li>
+
+                 </ol>
             </div>
         </div>
         <div></div>
